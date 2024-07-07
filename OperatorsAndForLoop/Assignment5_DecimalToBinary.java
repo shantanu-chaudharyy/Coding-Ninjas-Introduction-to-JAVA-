b9040cf173b612d5eae5defacc1d99f59a1f601b
Shantanu chaudhary <shantanu.com01@gmail.com>
2024-07-07 11:57:13 +0530

    Initial Commit

--- OperatorsAndForLoop/Assignment5_DecimalToBinary.java
+++ OperatorsAndForLoop/Assignment5_DecimalToBinary.java
@@ -1,14 +1,5 @@
 //Problem statement
 //Given a decimal number (integer N), convert it into binary and print.
-//
-//Note for C++ coders:
-//Do not use the inbuilt implementation of "pow" function. 
-//The implementation of that function is done for 'double's and 
-//it may fail when used for 'int's, 'long's, or 'long long's. 
-//Implement your own "pow" function to work for non-float data types. 
-//The given input number could be large, 
-//so the corresponding binary number can exceed the integer range.
-//So you may want to take the answer as long for CPP and Java. 
 
 package OperatorsAndForLoop;
 
